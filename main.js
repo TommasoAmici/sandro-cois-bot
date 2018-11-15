ebbd2c3ee1d7ae8698774215a96ba54065136e90
Tommaso A <tommaso.amici@me.com>
2018-11-15 22:36:27 +0100

    plugins in one file again

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-const TelegramBot = require("node-telegram-bot-api");
+import TelegramBot from "node-telegram-bot-api";
+import googleImages from "./plugins/googleImages";
+import magic8ball from "./plugins/magic8Ball";
 
 // replace the value below with the Telegram token you receive from @BotFather
 const telegramToken = "";
 
 // Create a bot that uses 'polling' to fetch new updates
-export const bot = new TelegramBot(telegramToken, { polling: true });
+const bot = new TelegramBot(telegramToken, { polling: true });
+
+// Matches "!i [whatever]"
+bot.onText(/!i (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+  const chatId = msg.chat.id;
+  const img = googleImages(msg, match);
+  bot.sendPhoto(chatId, img);
+});
+
+// Matches "!i [whatever]"
+bot.onText(/\/magic8ball (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, magic8ball());
+});
