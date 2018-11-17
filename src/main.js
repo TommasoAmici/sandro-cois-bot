687a1cf36d5549061cdd6b7b77c82e7564f79ef0
Tommaso A <tommaso.amici@me.com>
2018-11-17 12:44:20 +0100

    nsfw plugin

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const googleImages = require("./plugins/googleImages");
 const magic8ball = require("./plugins/magic8Ball");
 const weather = require("./plugins/weather");
 const gago = require("./plugins/9gago");
+const nsfw = require("./plugins/nsfw");
 const cfg = require("./config");
 
 // Create a bot that uses 'polling' to fetch new updates
@@ -12,3 +13,4 @@ bot.onText(/!i (.+)/, googleImages(bot));
 bot.onText(/\/magic8ball/, magic8ball(bot));
 bot.onText(/\/weather (.+)/, weather(bot));
 bot.onText(/\/9gago/, gago(bot));
+bot.onText(/!nsfw/, nsfw(bot));
---
+++ src/plugins/nsfw.js
@@ -0,0 +1,6 @@
+module.exports = bot => (msg, _) => {
+  const chatId = msg.chat.id;
+  const nsfw =
+    "NSFW\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nNSFW";
+  bot.sendMessage(chatId, nsfw);
+};
