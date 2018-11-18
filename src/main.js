be5eb1ff17773b357daa292c00fb2668fd304381
Tommaso A <tommaso.amici@me.com>
2018-11-18 21:37:45 +0100

    fix regex for images and gifs

--- src/main.js
+++ src/main.js
@@ -32,12 +32,12 @@ const markovWriteStream = fs.createWriteStream("markov.txt", { flags: "a" });
 
 bot.onText(/^!i (.+)/i, googleImages(bot, markovWriteStream));
 bot.onText(
-  /^[^(http)](.+).(png|jpg|jpeg|tiff|stk)$/i,
+  /^(?!.*http)(.+)\.(png|jpg|jpeg|tiff|stk)$/i,
   googleImages(bot, markovWriteStream)
 );
 bot.onText(/^!gif (.+)/i, giphy(bot, markovWriteStream));
 bot.onText(
-  /^[^(http)](.+).(gif|webm|mp4|gifv)$/i,
+  /^(?!.*http)(.+)\.(gif|webm|mp4|gifv)$/i,
   giphy(bot, markovWriteStream)
 );
 bot.onText(/^\/magic8ball/i, magic8ball(bot));
