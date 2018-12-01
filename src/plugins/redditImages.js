254aac2726615eec1288fa046934c1b16a8defde
Tommaso A <tommaso.amici@me.com>
2018-12-01 19:43:11 +0100

    better handling of reddit links
    selects correct method for videos, gifs, pictures, links etc

--- src/plugins/redditImages.js
+++ src/plugins/redditImages.js
@@ -20,7 +20,43 @@ module.exports = bot => async (msg, match) => {
       bot.sendMessage("Nothing found.");
     } else {
       const item = utils.randomChoice(response.data.data.children);
-      bot.sendPhoto(chatId, item.data.url);
+      // find correct api method
+      // images
+      if (item.data.post_hint === "image") {
+        bot.sendPhoto(chatId, item.data.url);
+      }
+      // hosted on reddit
+      else if (item.data.post_hint === "hosted:video") {
+        bot.sendVideo(chatId, item.data.media.reddit_video.fallback_url);
+      }
+      // external video hosting
+      else if (item.data.post_hint === "rich:video") {
+        // gfycat sucks
+        if (item.data.domain === "gfycat.com") {
+          bot.sendVideo(
+            chatId,
+            item.data.preview.reddit_video_preview.fallback_url
+          );
+        } else {
+          bot.sendVideo(chatId, item.data.url);
+        }
+      }
+      // links
+      else if (item.data.post_hint === "link") {
+        bot.sendMessage(
+          chatId,
+          `${item.data.title}\n\nhttps://old.reddit.com${
+            item.data.permalink
+          }\n\n${item.data.url}`
+        );
+      }
+      // self text
+      else {
+        bot.sendMessage(
+          chatId,
+          `${item.data.title}\n\nhttps://old.reddit.com${item.data.permalink}`
+        );
+      }
     }
   } catch (error) {
     if (error.response && error.response.status >= 400) {
