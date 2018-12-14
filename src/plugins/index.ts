4fc4c81b8c336f0de14f9d9c47782f7a632437e7
Tommaso A <tommaso.amici@me.com>
2018-12-14 18:40:20 +0100

    fix windows \r\n

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,24 +1,24 @@
 // plugins
-import gago from './9gago';
-import calc from './calc';
-import get from './get';
-import gifs from './gifs';
-import googleImages from './googleImages';
-import loc from './loc';
-import magic8ball from './magic8Ball';
-import nsfw from './nsfw';
-import pokedex from './pokedex';
-import printStats from './printStats';
-import quotes from './quotes';
-import reddit from './reddit';
-import roll from './roll';
-import set from './set';
-import spongebob from './spongebob';
-import stats from './stats';
-import stickers from './stickers';
-import unset from './unset';
-import weather from './weather';
-import what from './what';
+import gago from "./9gago";
+import calc from "./calc";
+import get from "./get";
+import gifs from "./gifs";
+import googleImages from "./googleImages";
+import loc from "./loc";
+import magic8ball from "./magic8Ball";
+import nsfw from "./nsfw";
+import pokedex from "./pokedex";
+import printStats from "./printStats";
+import quotes from "./quotes";
+import reddit from "./reddit";
+import roll from "./roll";
+import set from "./set";
+import spongebob from "./spongebob";
+import stats from "./stats";
+import stickers from "./stickers";
+import unset from "./unset";
+import weather from "./weather";
+import what from "./what";
 
 export default {
   gago,
@@ -40,5 +40,5 @@ export default {
   stickers,
   unset,
   weather,
-  what,
-}
+  what
+};
--- src/plugins/stickers/index.ts
+++ src/plugins/stickers/index.ts
@@ -1,6 +1,6 @@
-import get from './get';
-import setKey from './setKey';
-import setSticker from './setSticker';
-import unset from './unset';
+import get from "./get";
+import setKey from "./setKey";
+import setSticker from "./setSticker";
+import unset from "./unset";
 
-export default {setKey, setSticker, get, unset};
+export default { setKey, setSticker, get, unset };
