573ca19dcd3af79d8643b4e8c9791577103db62d
Tommaso A <tommaso.amici@me.com>
2019-02-09 18:57:04 +0100

    !i on reply would not grab the correct query

--- src/plugins/images/index.ts
+++ src/plugins/images/index.ts
@@ -1,13 +1,14 @@
-import * as TelegramBot from 'node-telegram-bot-api';
-import getImage from './getImage';
+import * as TelegramBot from "node-telegram-bot-api";
+import getImage from "./getImage";
 
-export default (bot: TelegramBot) =>
-    (msg: TelegramBot.Message, match: RegExpMatchArray): void => {
-      let query: string;
-      if (msg.reply_to_message) {
-        query = msg.reply_to_message.text;
-      } else {
-        query = match[1];
-      }
-      getImage(query, bot, msg);
-    };
+export default (bot: TelegramBot) => (
+  msg: TelegramBot.Message,
+  match: RegExpMatchArray
+): void => {
+  let query = match[1];
+
+  if (query === null && msg.reply_to_message) {
+    query = msg.reply_to_message.text;
+  }
+  getImage(query, bot, msg);
+};
