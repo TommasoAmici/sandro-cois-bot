77dffe92420a9d0cfd1120d387037ec0b96d269d
Tommaso A <tommaso.amici@me.com>
2019-07-21 00:20:25 +0200

    added explanatory figure

--- src/plugins/naziMods.ts
+++ src/plugins/naziMods.ts
@@ -4,6 +4,8 @@ export default (bot: TelegramBot) => async (
     msg: TelegramBot.Message
 ): Promise<void> => {
     const admins = await bot.getChatAdministrators(msg.chat.id);
-    const nazis = admins.map(a => a.user.username).join('\n');
+    const nazis =
+        'Taking my mods for a walk ( ͡° ͜ʖ ͡°)╯╲___卐卐卐卐\n\n' +
+        admins.map(a => a.user.username).join('\n');
     bot.sendMessage(msg.chat.id, nazis);
 };
