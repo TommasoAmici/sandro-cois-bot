e5f0ae07f0538c157339927f505b3bbe33a97662
Tommaso A <tommaso.amici@me.com>
2019-07-21 10:17:42 +0200

    fixed: would only return a subset of users/quotes

--- src/plugins/quotes/get.ts
+++ src/plugins/quotes/get.ts
@@ -2,16 +2,28 @@ import * as TelegramBot from 'node-telegram-bot-api';
 import { sscanAsync } from '../../redisClient';
 import utils from '../utils';
 
+const getQuote = async (key, match) => {
+    const quotes = await sscanAsync(
+        key,
+        '0',
+        'match',
+        `*${match}*`,
+        'count',
+        '1000'
+    );
+    return quotes[1];
+};
+
 export default (bot: TelegramBot) => async (
     msg: TelegramBot.Message,
     match: RegExpMatchArray
 ): Promise<void> => {
     const key = `chat:${msg.chat.id}:quotes`;
-    const quotes = await sscanAsync(key, '0', 'match', `*${match[1]}*`);
+    const quotes = await getQuote(key, match[1]);
     if (quotes === undefined) {
         bot.sendMessage(msg.chat.id, 'No quote found :(');
     } else {
-        const quote = utils.randomChoice(quotes[1]);
+        const quote = utils.randomChoice(quotes);
         bot.sendMessage(msg.chat.id, quote);
     }
 };
--- src/plugins/stats/print.ts
+++ src/plugins/stats/print.ts
@@ -1,5 +1,5 @@
 import * as TelegramBot from 'node-telegram-bot-api';
-import client, { scanAsync, hmgetAsync } from '../../redisClient';
+import { scanAsync, hmgetAsync } from '../../redisClient';
 
 interface User {
     count: number;
@@ -8,7 +8,13 @@ interface User {
 
 const getUsers = async (chatId: Number): Promise<User[]> => {
     let users: User[] = [];
-    const keys = await scanAsync('0', 'match', `chat:${chatId}:user:*`);
+    const keys = await scanAsync(
+        '0',
+        'match',
+        `chat:${chatId}:user:*`,
+        'count',
+        '1000'
+    );
     for (let key of keys[1]) {
         let user = await hmgetAsync(key, 'name', 'stats');
         users.push({ name: user[0], count: +user[1] });
