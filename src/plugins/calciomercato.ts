2c6c04fced93d4fd7c484eebebd3e8003733d0c6
Tommaso A <tommaso.amici@me.com>
2019-08-17 11:13:14 +0200

    added gazzetta to /cm providers

--- src/plugins/calciomercato.ts
+++ src/plugins/calciomercato.ts
@@ -9,7 +9,7 @@ const entities = new AllHtmlEntities();
 const url =
     'https://www.calciomercato.com/api/articles.html?limit=36&favourite_team=mercato&articleType=categoryNews';
 
-export default (bot: TelegramBot) => (msg: TelegramBot.Message) => {
+const calcioMercato = (bot: TelegramBot, msg: TelegramBot.Message) =>
     axios
         .get(url)
         .then(res => {
@@ -28,4 +28,47 @@ export default (bot: TelegramBot) => (msg: TelegramBot.Message) => {
             });
         })
         .catch(e => bot.sendMessage(msg.chat.id, '🤷🏻‍♂️'));
+
+const gazzettaUrl =
+    'https://components2.gazzettaobjects.it/rcs_gaz_searchapi/v1/latest.json';
+
+const includesCalcio = (arr: string[]): boolean => {
+    for (const a of arr) {
+        if (a.toLowerCase().includes('calcio')) return true;
+    }
+    return false;
+};
+
+interface Article {
+    image: string;
+    standFirst: string;
+    section: string[];
+    url: string;
+    headline: string;
+    [propsName: string]: string | string[];
+}
+
+const prepareString = (article: Article): string =>
+    `**${article.headline}**\n${article.standFirst}\n\n${article.url}`;
+
+const gazzetta = (bot: TelegramBot, msg: TelegramBot.Message) =>
+    axios
+        .get(gazzettaUrl)
+        .then(res => {
+            const calcioArticles = res.data.response.docs.filter((d: Article) =>
+                includesCalcio(d.section)
+            ) as Article[];
+            const article = utils.randomChoice(calcioArticles);
+            const articleString = prepareString(article);
+            bot.sendMessage(msg.chat.id, articleString, {
+                parse_mode: 'Markdown',
+            });
+        })
+        .catch(e => bot.sendMessage(msg.chat.id, '🤷🏻‍♂️'));
+
+const providers = [gazzetta, calcioMercato];
+
+export default (bot: TelegramBot) => (msg: TelegramBot.Message) => {
+    const provider = utils.randomChoice(providers);
+    provider(bot, msg);
 };
