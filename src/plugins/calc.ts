5a0d5b80059c4eb25eb7707a3e5c41ae3dd20880
Tommaso Amici <tommaso.amici@me.com>
2021-06-13 11:49:45 +0200

    Fixed import of mathjs evaluate

--- src/plugins/calc.ts
+++ src/plugins/calc.ts
@@ -1,37 +1,35 @@
-import math from "mathjs";
+import { evaluate } from "mathjs";
 import * as TelegramBot from "node-telegram-bot-api";
 
-export default (bot: TelegramBot) => (
-  msg: TelegramBot.Message,
-  match: RegExpMatchArray
-): void => {
-  try {
-    const evaluated = math.eval(match[1]);
-    if (typeof evaluated === "object") {
-      // handle conversions
-      if (match[1].includes(" to ")) {
-        bot.sendMessage(
-          msg.chat.id,
-          `${evaluated.value / evaluated.units[0].unit.value} ${
-            evaluated.units[0].unit.name
-          }`
-        );
+export default (bot: TelegramBot) =>
+  (msg: TelegramBot.Message, match: RegExpMatchArray): void => {
+    try {
+      const evaluated = evaluate(match[1]);
+      if (typeof evaluated === "object") {
+        // handle conversions
+        if (match[1].includes(" to ")) {
+          bot.sendMessage(
+            msg.chat.id,
+            `${evaluated.value / evaluated.units[0].unit.value} ${
+              evaluated.units[0].unit.name
+            }`
+          );
+        }
+        // handle calcs with different units
+        else {
+          bot.sendMessage(
+            msg.chat.id,
+            `${evaluated.value / evaluated.units[0].prefix.value} ${
+              evaluated.units[0].prefix.name
+            }${evaluated.units[0].unit.name}`
+          );
+        }
       }
-      // handle calcs with different units
+      // numerical expression needs no handling
       else {
-        bot.sendMessage(
-          msg.chat.id,
-          `${evaluated.value / evaluated.units[0].prefix.value} ${
-            evaluated.units[0].prefix.name
-          }${evaluated.units[0].unit.name}`
-        );
+        bot.sendMessage(msg.chat.id, evaluated);
       }
+    } catch (error) {
+      bot.sendMessage(msg.chat.id, "🤷🏻‍♂️");
     }
-    // numerical expression needs no handling
-    else {
-      bot.sendMessage(msg.chat.id, evaluated);
-    }
-  } catch (error) {
-    bot.sendMessage(msg.chat.id, "🤷🏻‍♂️");
-  }
-};
+  };
