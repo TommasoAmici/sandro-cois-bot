323940c738deac69f1c2a623a8b17b8e4f73ba12
Tommaso Amici <me@tommasoamici.com>
2022-08-21 17:18:13 +0200

    refactor(calciomercato): use undici instead of axios

--- packages/bot/src/plugins/calciomercato.ts
+++ packages/bot/src/plugins/calciomercato.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { decode } from "html-entities";
 import { parse } from "node-html-parser";
 import TelegramBot from "node-telegram-bot-api";
@@ -8,25 +7,22 @@ import { randomChoice } from "./utils/random";
 const url =
   "https://www.calciomercato.com/api/articles.html?limit=36&favourite_team=mercato&articleType=categoryNews";
 
-const calcioMercato = (bot: TelegramBot, msg: TelegramBot.Message) =>
-  axios
-    .get<any>(url)
-    .then(res => {
-      const article = randomChoice(
-        (parse(res.data).childNodes as any[]).filter(
-          (a: any) => a.tagName === "article",
-        ),
-      );
-      const item = article
-        .querySelector(".news-item__extract")
-        .removeWhitespace();
-      const href = item.attributes.href;
-      const text = decode(item.childNodes[0].rawText);
-      bot.sendMessage(msg.chat.id, `${text}\n${href}`, {
-        parse_mode: "HTML",
-      });
-    })
-    .catch(e => bot.sendMessage(msg.chat.id, "🤷🏻‍♂️"));
+const calcioMercato = async (bot: TelegramBot, msg: TelegramBot.Message) => {
+  const res = await request(url);
+  const data = await res.body.text();
+
+  const article = randomChoice(
+    (parse(data).childNodes as any[]).filter(
+      (a: any) => a.tagName === "article",
+    ),
+  );
+  const item = article.querySelector(".news-item__extract").removeWhitespace();
+  const href = item.attributes.href;
+  const text = decode(item.childNodes[0].rawText);
+  bot.sendMessage(msg.chat.id, `${text}\n${href}`, {
+    parse_mode: "HTML",
+  });
+};
 
 const gazzettaURL = "https://searchapiservice2.gazzetta.it/api/section/calcio";
 
