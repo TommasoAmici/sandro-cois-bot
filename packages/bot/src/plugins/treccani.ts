0e1e4f689043d27c535c34eac1186c7308a1ec22
Tommaso Amici <me@tommasoamici.com>
2022-08-21 17:19:14 +0200

    refactor(treccani): use undici instead of axios

--- packages/bot/src/plugins/treccani.ts
+++ packages/bot/src/plugins/treccani.ts
@@ -1,15 +1,20 @@
-import axios from "axios";
 import { parse } from "node-html-parser";
 import TelegramBot from "node-telegram-bot-api";
+import { request } from "undici";
 import { paginateMessages } from "./utils";
 
-const baseURL = "http://www.treccani.it";
+const baseURL = "https://www.treccani.it";
 
 const getDetailURL = async (word: string) => {
-  const listURL = `${baseURL}/vocabolario/ricerca/${word}`;
-  const res = await axios.get<string>(listURL);
-  if (res.status === 404) return undefined;
-  const root = parse(res.data);
+  const listURL = `${baseURL}/vocabolario/ricerca/${word}/`;
+  const res = await request(listURL);
+  const text = await res.body.text();
+
+  if (res.statusCode === 404 || text === "") {
+    return undefined;
+  }
+
+  const root = parse(text);
   const firstDefinition = root.querySelector(
     "section.module-article-search_preview",
   );
@@ -26,8 +31,9 @@ export default (bot: TelegramBot) =>
     const detailURL = await getDetailURL(match[1]);
 
     if (detailURL !== undefined) {
-      const res = await axios.get<string>(detailURL);
-      const root = parse(res.data);
+      const res = await request(detailURL);
+      const text = await res.body.text();
+      const root = parse(text);
       const section = root.querySelector("div.text.spiega");
       paginateMessages(bot, msg, section.rawText);
     } else {
