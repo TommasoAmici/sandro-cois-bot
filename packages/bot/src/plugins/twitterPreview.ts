d7a2c07d52dafb549639c4e82423c25c4060ad95
Tommaso Amici <me@tommasoamici.com>
2022-10-03 19:23:57 +0200

    fix(twitter): entities only provide length and offset properties

--- packages/bot/src/plugins/twitterPreview.ts
+++ packages/bot/src/plugins/twitterPreview.ts
@@ -5,14 +5,21 @@ import TelegramBot from "node-telegram-bot-api";
  */
 export default (bot: TelegramBot) =>
   (msg: TelegramBot.Message): void => {
+    const sendTweet = (url: URL) => {
+      if (url.host === "twitter.com") {
+        url.search = "";
+        url.hostname = "vxtwitter.com";
+        bot.sendMessage(msg.chat.id, url.toString());
+      }
+    };
+
     msg.entities?.forEach(e => {
       if (e.type === "url") {
+        const url = new URL(msg.text.slice(e.offset, e.offset + e.length));
+        sendTweet(url);
+      } else if (e.type === "text_link") {
         const url = new URL(e.url);
-        if (url.host === "twitter.com") {
-          url.search = "";
-          url.hostname = "vxtwitter.com";
-          bot.sendMessage(msg.chat.id, url.toString());
-        }
+        sendTweet(url);
       }
     });
   };
