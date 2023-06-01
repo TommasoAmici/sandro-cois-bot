e8bc42761a293e6133a27b53b01c5e5f5679a7c6
Tommaso Amici <me@tommasoamici.com>
2023-06-01 19:02:47 +0200

    fix: kickChatMember is not a valid API function anymore

--- packages/bot/src/plugins/telegramPremium.ts
+++ packages/bot/src/plugins/telegramPremium.ts
@@ -2,7 +2,7 @@ import TelegramBot from "node-telegram-bot-api";
 
 export default (bot: TelegramBot) =>
   (msg: TelegramBot.Message): void => {
-    bot.kickChatMember(msg.chat.id, String(msg.from.id));
-    bot.unbanChatMember(msg.chat.id, String(msg.from.id));
+    bot.banChatMember(msg.chat.id, msg.from.id);
+    bot.unbanChatMember(msg.chat.id, msg.from.id);
     bot.sendSticker(msg.chat.id, "CAADBAADxAADuChICFX6VrCSrzkLAg");
   };
