2dbe15a97c580c992a2213113cba38e2a349e28c
Tommaso Amici <me@tommasoamici.com>
2023-06-01 19:06:59 +0200

    feat: replaced g-i-s with googlethis

--- packages/bot/src/plugins/getImage.ts
+++ packages/bot/src/plugins/getImage.ts
@@ -1,29 +1,28 @@
 import TelegramBot from "node-telegram-bot-api";
 import { randomChoice } from "./utils/random";
 
-const gis = require("g-i-s");
+import google from "googlethis";
 
 export const getImage = async (
   query: string,
   bot: TelegramBot,
   msg: TelegramBot.Message,
 ): Promise<void> => {
-  gis(query, (error, results) => {
-    if (error) {
-      bot.sendMessage(msg.chat.id, error);
+  try {
+    const images = await google.image(query, { safe: false });
+    if (!images || images.length === 0) {
+      bot.sendMessage(msg.chat.id, "No photo found.", {
+        reply_to_message_id: msg.message_id,
+      });
     } else {
-      if (!results || results.length === 0) {
-        bot.sendMessage(msg.chat.id, "No photo found.", {
-          reply_to_message_id: msg.message_id,
-        });
-      } else {
-        const item = randomChoice(results as ImageItem[]);
-        bot.sendPhoto(msg.chat.id, item.url, {
-          reply_to_message_id: msg.message_id,
-        });
-      }
+      const item = randomChoice(images);
+      bot.sendPhoto(msg.chat.id, item.url, {
+        reply_to_message_id: msg.message_id,
+      });
     }
-  });
+  } catch (error) {
+    bot.sendMessage(msg.chat.id, String(error));
+  }
 };
 
 export default (bot: TelegramBot) =>
