ff190d0e8a7556cc2ea5d063baec8b9931c866dd
Tommaso Amici <me@tommasoamici.com>
2023-06-09 08:33:26 +0200

    refactor: import using path alias instead of relative imports

--- packages/bot/src/middleware.ts
+++ packages/bot/src/middleware.ts
@@ -1,5 +1,10 @@
 import { NextFunction } from "grammy";
 
+/**
+ * Factory function to create a grammy middleware.
+ * It should be used to wrap all handlers when they are registered.
+ * This makes sure that messages are always handled by all registered handlers.
+ */
 export function middlewareFactory<T>(
   handler: (ctx: T) => Promise<void> | void,
 ) {
