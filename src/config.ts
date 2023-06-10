1a324285533069f1a0528c8c8022e9041a52c797
Tommaso Amici <me@tommasoamici.com>
2023-06-10 15:55:22 +0200

    chore: removed unused environment variables

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,5 @@
 export default {
   telegramToken: process.env.TELEGRAM_TOKEN,
-  googleCseToken: process.env.GOOGLE_CSE_TOKEN,
-  googleApiToken: process.env.GOOGLE_API_TOKEN,
   openWeatherToken: process.env.OPENWEATHER_TOKEN,
   giphyToken: process.env.GIPHY_TOKEN,
   mapboxToken: process.env.MAPBOX_TOKEN,
