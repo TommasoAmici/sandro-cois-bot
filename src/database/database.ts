d7db2692d930f2c0143eb5854dfd48437d1c56a5
Tommaso Amici <me@tommasoamici.com>
2023-06-10 13:32:18 +0200

    feat: load sqlite extensions from SQLITE_EXTENSIONS env variable

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,3 +3,10 @@ import { Database } from "bun:sqlite";
 export const db = new Database(process.env.DATABASE_PATH ?? "bot.sqlite", {
   create: true,
 });
+
+if (process.env.SQLITE_EXTENSIONS) {
+  const extensions = process.env.SQLITE_EXTENSIONS.split(",");
+  for (const extension of extensions) {
+    db.loadExtension(extension);
+  }
+}
